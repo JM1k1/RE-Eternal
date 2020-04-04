21a6290a0521186a95c749537adb3892c0e5ea22
JMiki <themrjmiki@gmail.com>
2020-04-04 15:09:07 +0300

    refactor: (refactoring vk post check func)

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,23 @@
 const { readFileSync, writeFile } = require("fs");
-const snek = require("node-superfetch");
+var lastId = readFileSync("storage/lastId.db", "utf8");
+
 module.exports = async (client) => {
   console.log(`${client.user.tag} is online.`);
   client.user.setActivity(`Тут пока что пусто.`, { type: "PLAYING" });
-  setInterval(async () => await checkVkPost(client), 1000 * 30);
+  setInterval(async () => await checkVkPost(client), 1000 * 1);
 };
 
 async function checkVkPost(client) {
-  const lastId = readFileSync("storage/lastId.db", "utf8");
-  const post = await getPostId();
-  if (lastId < post.id) {
-    require("./vknews")(client, post);
-    writeFile("storage/lastId.db", post.id, (err) =>
-      err ? console.log(err) : err
-    );
-  }
-}
-
-async function getPostId() {
-  const { body } = await snek.get(
+  const { body } = await client.snek.get(
     `https://api.vk.com/method/wall.get?domain=replay_spb&count=2&v=5.52&access_token=${process.env.VK_TOKEN}`
   );
+  let post;
   if (body.response.items[0].date < body.response.items[1].date)
-    return body.response.items[1];
-  else return body.response.items[0];
+    post = body.response.items[1];
+  else post = body.response.items[0];
+  if (lastId < post.id) {
+    require("./vknews")(client, post);
+    lastId = post.id;
+    writeFile("storage/lastId.db", post.id, (err) => err);
+  }
 }
