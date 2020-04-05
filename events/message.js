f18236cf3beac43cd61cd80b707f39419a41fc50
JMiki <themrjmiki@gmail.com>
2020-04-05 19:25:21 +0300

    Miss fix

--- events/message.js
+++ events/message.js
@@ -1,8 +1,8 @@
 const { prefix } = require("../config.json");
 
-module.exports = (client, msg) => {
+module.exports = async (client, msg) => {
   if (msg.author.bot || !msg.guild) return;
-  //if (!msg.member && msg.guild) msg.member = await msg.guild.members.fetch(msg);
+  if (!msg.member && msg.guild) msg.member = await msg.guild.members.fetch(msg);
   if (msg.content.startsWith(prefix))
     return require("../handle/command.js")(client, msg);
 };
